caa623bc1b25a4784aba9a7646e56c844edf04a1
Alex Iadicicco <alex@theta4.com>
2012-03-31 16:40:45 -0700

    Remove libunicorn list and dict API

--- src/unicorn.h
+++ src/unicorn.h
@@ -5,81 +5,6 @@
 #include <stddef.h>
 
 
-/* src/list.c */
-
-/*
- * I'm not going to lie: a lot about how this linked list API works is
- * heavily borrowed from libmowgli. I just really like it, I guess.
- */
-
-struct irc_node_ {
-        struct irc_node_ *next, *prev;
-        void *data;
-};
-struct irc_list_ {
-        struct irc_node_ *head, *tail;
-        unsigned count;
-};
-
-typedef struct irc_node_ irc_node_t;
-typedef struct irc_list_ irc_list_t;
-
-#define irc_list_for_each(curr, list) \
-        for((curr) = (list)->head; (curr) != NULL; (curr) = (curr)->next)
-#define irc_list_for_each_safe(curr, next, list) \
-        for((curr) = (list)->head, (next) = (curr) ? (curr)->next : NULL; \
-                (curr) != NULL; \
-                (curr) = (next), (next) = (curr) ? (curr)->next : NULL)
-
-extern irc_node_t *irc_node_create(); // malloc/memset wrapper :/
-extern void irc_node_free(irc_node_t *node); // free wrapper :/
-extern int irc_node_add(void *data, irc_node_t *node, irc_list_t *list);
-extern int irc_node_delete(irc_node_t *node, irc_list_t *list);
-
-
-/* src/dict.c */
-
-/*
- * Yeah, this is borrowed from libmowgli also...
- */
-
-#define IRC_DICT_TYPE_LEAF 0
-#define IRC_DICT_TYPE_NODE -1
-
-union irc_dict_elem_;
-struct irc_dict_leaf_ {
-        int type;
-        union irc_dict_elem_ *parent;
-        char *canonized;
-        void *data;
-};
-struct irc_dict_node_ {
-        int type;
-        union irc_dict_elem_ *parent;
-        union irc_dict_elem_ *child[16];
-};
-union irc_dict_elem_ {
-        int type;
-        struct irc_dict_leaf_ leaf;
-        struct irc_dict_node_ node;
-};
-struct irc_dict_ {
-        void (*canonize)(char *key);
-        union irc_dict_elem_ *root;
-};
-typedef struct irc_dict_leaf_ irc_dict_leaf_t;
-typedef struct irc_dict_node_ irc_dict_node_t;
-typedef union irc_dict_elem_ irc_dict_elem_t;
-typedef struct irc_dict_ irc_dict_t;
-
-extern irc_dict_elem_t *irc_dict_leaf_create(char *canonized);
-extern irc_dict_elem_t *irc_dict_node_create();
-extern void irc_dict_elem_free(irc_dict_elem_t *elem);
-extern int irc_dict_add(irc_dict_t *dict, const char *key, void *data);
-extern void *irc_dict_get(irc_dict_t *dict, const char *key);
-extern int irc_dict_delete(irc_dict_t *dict, const char *key);
-
-
 /*
  * NOTE: the formatter functions (irc_sender_format and
  * irc_message_format) ask for a buffer size argument. It wouldn't be
@@ -126,7 +51,7 @@ struct irc_message_ {
         char *command;
 
         /* arguments */
-        irc_list_t args;
+        mowgli_list_t args;
 };
 typedef struct irc_message_ irc_message_t;
 
@@ -145,7 +70,7 @@ struct irc_mode_ops_ {
 };
 typedef struct irc_mode_ops_ irc_mode_ops_t;
 
-extern int irc_mode_parse(irc_mode_ops_t *ops, char *modespec, char *argmodes, irc_node_t *args);
+extern int irc_mode_parse(irc_mode_ops_t *ops, char *modespec, char *argmodes, mowgli_node_t *args);
 
 
 /* src/nick.c */
